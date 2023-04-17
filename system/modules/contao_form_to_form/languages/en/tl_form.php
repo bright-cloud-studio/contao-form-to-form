c909e45ca5848dd796f7e95910a3aa954de13569
Bright Cloud Studio <84467517+bright-cloud-studio@users.noreply.github.com>
2023-04-17 11:22:52 -0400

    Create tl_form.php

---
+++ system/modules/contao_form_to_form/languages/en/tl_form.php
@@ -0,0 +1,17 @@
+<?php
+
+/**
+ * Bright Cloud Studio's Contao Form-to-Form
+ *
+ * Copyright (C) 2023 Bright Cloud Studio
+ *
+ * @package    bright-cloud-studio/contao-form-to-form
+ * @link       https://www.brightcloudstudio.com/
+ * @license    http://opensource.org/licenses/lgpl-3.0.html
+**/
+
+/* Legends */
+$GLOBALS['TL_LANG']['tl_form']['cftf_legend'] = 'Form-to-Form Settings';
+
+/* Fields */
+$GLOBALS['TL_LANG']['tl_form']['cftf_target']          = array('Form-to-Form Target', 'Select the target form this will lead into');
