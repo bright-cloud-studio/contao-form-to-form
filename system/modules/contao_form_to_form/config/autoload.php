a1076b27f40617d914daa2f5278e99788cc0a8a3
Bright Cloud Studio <84467517+bright-cloud-studio@users.noreply.github.com>
2023-04-17 10:48:14 -0400

    Create autoload.php

---
+++ system/modules/contao_form_to_form/config/autoload.php
@@ -0,0 +1,18 @@
+<?php
+ 
+/**
+ * Bright Cloud Studio's Contao Form-to-Form
+ *
+ * Copyright (C) 2023 Bright Cloud Studio
+ *
+ * @package    bright-cloud-studio/contao-form-to-form
+ * @link       https://www.brightcloudstudio.com/
+ * @license    http://opensource.org/licenses/lgpl-3.0.html
+**/
+
+ 
+/* Register the classes */
+ClassLoader::addClasses(array
+(
+  'Bcs\ContaoFormToForm' => 'system/modules/contao_form_to_form/library/Bcs/ContaoFormToForm.php',
+));
