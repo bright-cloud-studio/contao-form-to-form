5444fa5e1fd90dd50f17bd967ea0d2e13c2c5cda
Bright Cloud Studio <84467517+bright-cloud-studio@users.noreply.github.com>
2023-04-17 12:09:57 -0400

    Update ContaoFormToForm.php

--- system/modules/contao_form_to_form/library/Bcs/ContaoFormToForm.php
+++ system/modules/contao_form_to_form/library/Bcs/ContaoFormToForm.php
@@ -15,19 +15,23 @@ namespace Bcs;
 class ContaoFormToForm 
 {
 
-  public function prefillMatchingFields($arrFields, $formId, $objForm)
-  {
-    if (substr($formId, 0, 10) == "auto_form_" && \Input::post('preload_form') != '') {
-      if ($objForm->id == \Input::post('preload_form')) {
-        foreach ($arrFields AS $objField)
-        {
-          if (\Input::post($objField->name) != '') {
-            $objField->value = \Input::post($objField->name);
-          }
-        }	
-      }
+    public function prefillMatchingFields($arrFields, $formId, $objForm)
+    {
+        // if $_POST contains our hidden target field
+        if (\Input::post('form_to_form_target') != '') {
+            // if the target ID matches this form's ID
+            if (\Input::post('form_to_form_target') == $objForm->id) {
+                // loop through all of the form fields
+                foreach ($arrFields as $field)
+                {
+                    // if we have a value in post that matches this field's name
+                    if (\Input::post($field->name) != '') 
+                        $field->value = \Input::post($field->name);
+                }	
+            }
+        }
+        // return our modified form fields
+        return $arrFields;
     }
-    return $arrFields;
-  }
 
 }
